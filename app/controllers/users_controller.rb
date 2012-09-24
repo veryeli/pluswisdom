57e1ae0eef1c7ad8fec19ad9bdf8aa29d7ef6329
Eliana Feasley <e.feasley@gmail.com>
2012-09-24 14:43:32 -0500

    Now you can sign up. For something. And I know a guy with a cloud on sale.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
   def new
+    @user = User.new
+  end
+    def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Thanks for joining us!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Eliana Feasley"
+    email    "e.feasley@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
