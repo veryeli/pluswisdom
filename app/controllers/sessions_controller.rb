97120e9fe68884a85e116758d6dc3129e610f4b9
Eliana Feasley <e.feasley@gmail.com>
2012-09-25 23:26:51 -0500

    Now the users can have all sorts of stuff happen like edit, update, index, and destroy.  NOT A restful chapter, but a RESTful one

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,17 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+ def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
-      flash.now[:error] = "Invalid: Maybe we don't know you or maybe that was the wrong password."
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
 
   def destroy
     sign_out
